bbabb583a12b01acfaa0bd914254ee1e420f3416
• 𝐒 𝚰 𝚲 • 💗 / ‹𝟹 🌷 <siyatechbot@gmail.com>
2025-12-13 19:05:16 +0530

    Update _inline.py
    Signed-off-by: • 𝐒 𝚰 𝚲 • 💗 / ‹𝟹 🌷 <siyatechbot@gmail.com>

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -37,6 +37,8 @@ class Inline:
                     self.ikb(text="⥁", callback_data=f"controls replay {chat_id}"),
                     self.ikb(text="‣‣I", callback_data=f"controls skip {chat_id}"),
                     self.ikb(text="▢", callback_data=f"controls stop {chat_id}"),
+                ]
+                [
                     self.ikb(text="close", callback_data="help close"),
                 ]
             )
